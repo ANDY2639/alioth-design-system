fef3e5fe7891288e161daf3e1917dca97dd596ca
Andres Guerrero <andres.guerrero@ppm.com.ec>
2026-01-27 14:05:38 -0500

    Update typography componet export

---
+++ src/presentation/components/typography/index.ts
@@ -0,0 +1,6 @@
+export { Body, type BodyProps } from "./Body";
+export { Caption, type CaptionProps } from "./Caption";
+export { Heading, type HeadingProps } from "./Heading";
+export { Link, type LinkProps } from "./Link";
+export { List, type ListProps } from "./List";
+export { Note, type NoteProps } from "./Note";
